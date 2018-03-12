6bc424a9eeb3b4e4a8dfbfcb4d50cceaf5408f61
B2Bharat <34412573+B2Bharat@users.noreply.github.com>
2018-03-12 11:35:49 +0530

    12 march
    12 march

---
+++ 12march/server changes/testmail.php
@@ -0,0 +1,59 @@
+<?php
+/*require_once('phpmailer/PHPMailerAutoload.php');
+$mail = new PHPMailer(); // create a new object
+$mail->IsSMTP(); // enable SMTP
+$mail->SMTPDebug = 1; // debugging: 1 = errors and messages, 2 = messages only
+$mail->SMTPAuth = true; // authentication enabled
+$mail->SMTPSecure = 'tls'; // secure transfer enabled REQUIRED for Gmail
+$mail->Host = "smtp.gmail.com";
+$mail->Port = 587; // or 587
+$mail->IsHTML(true);
+$mail->Username = "vineet191085@gmail.com";
+$mail->Password = "Nokhe@1234";
+$mail->SetFrom("santosh.bhosale123@gmail.com");
+$mail->Subject = "Test";
+$mail->Body = "hello";
+$mail->AddAddress("santosh.bhosale123@gmail.com");
+
+ if(!$mail->Send()) {
+    echo "Mailer Error: " . $mail->ErrorInfo;
+ } else {
+    echo "Message has been sent";
+ }
+*/
+
+
+$to = "santosh.bhosale123@gmail.com";
+$subject = "HTML email";
+
+$message = "
+<html>
+<head>
+<title>HTML email</title>
+</head>
+<body>
+<p>This email contains HTML Tags!</p>
+<table>
+<tr>
+<th>Firstname</th>
+<th>Lastname</th>
+</tr>
+<tr>
+<td>John</td>
+<td>Doe</td>
+</tr>
+</table>
+</body>
+</html>
+";
+
+// Always set content-type when sending HTML email
+$headers = "MIME-Version: 1.0" . "\r\n";
+$headers .= "Content-type:text/html;charset=UTF-8" . "\r\n";
+
+// More headers
+$headers .= 'From: <admin@b2bharat.com>' . "\r\n";
+
+
+mail($to,$subject,$message,$headers);
+?>
