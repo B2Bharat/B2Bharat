126c805b7d66e4222a726c3c61c39a64f365d851
B2Bharat <34412573+B2Bharat@users.noreply.github.com>
2018-03-30 16:41:36 +0530

    bckp area

---
+++ bckp/state_ajax.php
@@ -0,0 +1,12 @@
+<? include "AMframe/config.php";
+ob_start(); 
+$val=isSet($val) ? $val : '' ;
+$state = "<option value=''>Select</option>";
+$DropDownQry = "SELECT state_id,state_name from states where country_id='$val' order by state_name asc";
+$state .= $drop->get_dropdown($db,$DropDownQry,NULL);
+?>
+<select class="form-control" name="state" id="state" Onchange="return get_city(this.value);">
+						<? echo $state;?>
+                     </select>
+
+
